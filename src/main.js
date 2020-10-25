a6c17b25caa71b7b7fb50524ad4a624fe3e3c4aa
bulb <pumpkinjack@livemail.tw>
2020-10-25 16:14:35 +0800

    加入 localforage

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import localforage from 'localforage'
 
 import '@CSS/main.scss'
 
 Vue.config.productionTip = false
+
+localforage.config({
+    name: 'mytube'
+})
 
 new Vue({
     router,
