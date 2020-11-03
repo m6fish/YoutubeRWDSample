0b7fa0f746e1068ffb356d18937c7907336a4bd5
bulb <pumpkinjack@livemail.tw>
2020-11-03 21:32:13 +0800

    VideoWall-RWD

--- src/views/Home/components/videoWall.vue
+++ src/views/Home/components/videoWall.vue
@@ -1,17 +1,14 @@
 <template>
     <div class="video-wall">
         <div v-for="(oneVideo, vidx) in getVideos" :key="`v${vidx}`" class="video-box" @click="goPlaying(oneVideo)">
-            <div>
-                <div class="v_pic">
-                    <img :src="oneVideo.pic">
-                    <span class="pic_duration">{{oneVideo.duration}}</span>
-                </div>
-                <div class="v_detail" :class="{active: isActive(oneVideo.id)}"  @click.stop="setFavo(oneVideo)">
-                    <div class="detail_des">{{oneVideo.title}}</div>
-                    <div class="detail_des">{{oneVideo.description}}</div>
-                    <div class="detail_favo">
-                        <i class="far fa-heart"/>
-                    </div>
+            <div class="v_pic" :style="{background: `url(${oneVideo.pic})`}">
+                <span class="pic_duration">{{oneVideo.duration}}</span>
+            </div>
+            <div class="v_detail" :class="{active: isActive(oneVideo.id)}"  @click.stop="setFavo(oneVideo)">
+                <div class="detail_des">{{oneVideo.title}}</div>
+                <div class="detail_des">{{oneVideo.description}}</div>
+                <div class="detail_favo">
+                    <i class="far fa-heart"/>
                 </div>
             </div>
         </div>
@@ -22,67 +19,89 @@
 $hoverColor: red;
 
 .video-wall {
-    text-align: center;
-    padding: 0 10%;
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+
+    @media screen and (min-width: 1745px) and (max-width: 1920px) {
+        padding: 0 10%;
+    }
+    @media screen and (min-width: 910px) and (max-width: 1085px) {
+        padding: 0 10%;
+    }
 
     .video-box {
-        box-sizing:border-box;
-        max-width: 25%;
-        display: inline-block;
+        margin: 10px 1%;
+        width: 23%;
         margin-bottom: 40px;
-        &+.video-box {
-            margin-left: 20px;
-        }
-    }
 
-    .v_pic {
-        position: relative;
-        cursor: pointer;
-        .pic_duration {
-            background: rgba(0,0,0,0.8);
-            color: #FFF;
-            border-radius: 2px;
-            padding: 2px 4px;
-            letter-spacing: 0.5px;
-            position: absolute;
-            bottom: 4px;
-            right: 0;
-            margin:4px;
-            font-size: 1rem;
-            line-height: 1rem;
+        @media screen and (max-width: 1425px) {
+            width: 31%;
+        }
+        @media screen and (max-width: 1085px) {
+            width: 48%;
+        }
+        @media screen and (max-width: 730px) {
+            width: 98%;
         }
 
-        &:hover{
+        .v_pic, .v_detail {
+            margin: 0 auto;
+            width: 320px;
+        }
+
+        .v_pic {
+            height: 180px;
+            position: relative;
+            cursor: pointer;
             .pic_duration {
-                color: $hoverColor;
+                background: rgba(0,0,0,0.8);
+                color: #FFF;
+                border-radius: 2px;
+                padding: 2px 4px;
+                letter-spacing: 0.5px;
+                position: absolute;
+                bottom: 4px;
+                right: 0;
+                margin:4px;
+                font-size: 1rem;
+                line-height: 1rem;
+            }
+
+            &:hover{
+                .pic_duration {
+                    color: $hoverColor;
+                }
             }
         }
-    }
 
-    .v_detail {
-        width: 320px;
+        .v_detail {
+            margin-top: 5px;
 
-        .detail_des {
-            width: 95%;
-            overflow: hidden;
-            white-space: nowrap;
-            text-overflow: ellipsis;
-            margin: 0;
-            display: inline-block;
-            font-size: 1rem;
-            font-weight: 500;
-            color: #030303;
-        }
-        .detail_favo {
-            float: right;
-            color:#CCC;
-        }
+            .detail_des {
+                width: 95%;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+                margin: 0;
+                display: inline-block;
+                font-size: 1rem;
+                font-weight: 500;
+                color: #030303;
+            }
+            .detail_favo {
+                float: right;
+                color:#CCC;
+            }
 
-        &:hover, &.active {
-            .detail_des, .detail_favo {
-                color: $hoverColor;
-                cursor: pointer;
-                transition: 0.2s;
+            &:hover, &.active {
+                .detail_des, .detail_favo {
+                    color: $hoverColor;
+                    cursor: pointer;
+                    transition: 0.2s;
+                }
             }
         }
     }
