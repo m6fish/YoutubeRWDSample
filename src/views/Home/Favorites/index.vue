b7bcb0d44f70ef6e2452d0c1b158b3ffde86fc56
bulb <pumpkinjack@livemail.tw>
2020-11-08 14:38:09 +0800

    Favo-加入空值頁面

--- src/views/Home/Favorites/index.vue
+++ src/views/Home/Favorites/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="Favorites">
-      <videoWall/>
-      <pageNum/>
-  </div>
+    <div class="Favorites">
+        <template v-if="getFavoIDs.length">
+            <videoWall/>
+            <pageNum/>
+        </template>
+        <div else class="empty">It's empty.</div>
+    </div>
 </template>
 
 <script>
-
 import videoWall from './videoWall'
 import pageNum from './pageNum'
 import Favo from '@S/Favorites'
 import { createNamespacedHelpers } from 'vuex'
 const STORE_NAME = 'Favo'
-const { mapActions } = createNamespacedHelpers(`${STORE_NAME}/`)
+const { mapGetters, mapActions } = createNamespacedHelpers(`${STORE_NAME}/`)
 
 export default {
     name: 'Favorites',
@@ -27,8 +29,10 @@ export default {
     beforeMount () {
         this.fetchFavorite()
     },
-    beforeDestroy () {
-        // this.$root.$emit('remove-store', [STORE_NAME])
+    computed: {
+        ...mapGetters([
+            'getFavoIDs'
+        ])
     },
     methods: {
         ...mapActions([
